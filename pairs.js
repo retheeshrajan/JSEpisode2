b976d2b4b85d03acf268488e020679fbaae40edf
unknown <retheesh@TENT-Retheesh.tamdeenrealestate.com>
2019-02-18 14:13:51 +0300

    Task2_Done_18Feb2019

--- pairs.js
+++ pairs.js
@@ -1,4 +1,23 @@
-(/**************************************************************
+/**********************************************
+* READ ME!!!!
+*
+* We've included this handy method for you.
+* It will remove a random element from an array
+* and return it to you.
+*
+* Example Usage:
+*
+* let numbers = [1, 2, 3, 4];
+* let random = numbers.getRandom();  // randomly returns something from the array. e.g. 3
+* console.log(random); // 3 (the random element)
+* console.log(numbers);  // [1, 2, 4] (missing the random element)
+************************************************/
+Array.prototype.getRandom = function () {
+  return this.splice(Math.floor(Math.random()*this.length), 1)[0];
+}
+
+
+/**************************************************************
 * pairs(names):
 *
 * - It accepts an array of names:
@@ -18,45 +37,54 @@
 *       pairs() returns []
 ****************************************************************/
 function pairs(names) {
-  // Your code goes here
-  let name1;
-  let name2;
-  let subarray = [];
-while (names.length > 1) {
-    name1 = names.getRandom();
-    name2 = names.getRandom();
-    subarray.push([name1, name2]);
-}
-if (names.length === 1)  {
-    name1 = names.getRandom();
-    subarray.push(name1);
-} else if (names.length === 0){
-    return names = []
-}
-return subarray; 
+  let name1='';
+  let name2='';
+  var myArray = []; var PArray= [];
+  if (names!==undefined)
+  {
+    var ln = names.length;
+    while(ln>0)
+    {
+      if (ln>=2)
+      {
+        name1=names.getRandom();
+        name2=names.getRandom();
+      }
+      else
+      {
+        name1=names.getRandom();
+        name1=name2;
+      }
+      
+      if (name1!==name2)
+      {
+        myArray.push(name1,name2);
+        ln=ln-2;
+      }
+      else
+      {
+        myArray.push(name1);
+        ln=ln-1;
+      }
+      PArray.push(myArray);
+      myArray=[];
+
+      
+    }
+  }
+  else
+  {
+    names=[];
+  }
+
+  return PArray;
 }
 
 
-// console.log(pairs(['retheesh','sarah']));
+
+//console.log(pairs(['retheesh','sarah']));
 
 module.exports = pairs;
 
-/**********************************************
-* READ ME!!!!
-*
-* We've included this handy method for you.
-* It will remove a random element from an array
-* and return it to you.
-*
-* Example Usage:
-*
-* let numbers = [1, 2, 3, 4];
-* let random = numbers.getRandom();  // randomly returns something from the array. e.g. 3
-* console.log(random); // 3 (the random element)
-* console.log(numbers);  // [1, 2, 4] (missing the random element)
-************************************************/
-Array.prototype.getRandom = function () {
-  return this.splice(Math.floor(Math.random()*this.length), 1)[0];
-}
 
 console.log(pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish', 'Hussein', 'Lailz', 'Mr Potato']));
